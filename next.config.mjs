d79fe0c2b938a6ee357eb7e1c737642f6b8ee84d
Shinji13 <keskasaymen8@gmail.com>
2024-06-21 12:14:35 +0100

    folder structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
+import { resolve } from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        "@": resolve(__dirname, "src"),
+        "@lib": resolve(__dirname, "src/lib"),
+        "@assets": resolve(__dirname, "src/assets"),
+        "@actions": resolve(__dirname, "src/actions"),
+        "@components": resolve(__dirname, "src/components"),
+        "@containers": resolve(__dirname, "src/containers"),
+        "@hooks": resolve(__dirname, "src/hooks"),
+        "@styles": resolve(__dirname, "src/styles"),
+      },
+    },
+  },
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,81 @@
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/lib/client/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      md: { max: "764px" },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primaryColor: "#FFC438",
+        backgroundColor: "#F9FAFA",
+        foregroundColor: "#454545",
+        dangerColor: "#ff0000",
+        successColor: "#27cf22",
+        mutedColor: "#505050",
+      },
+      borderRadius: {
+        bdr: "0.3rem",
+      },
+      fontFamily: {
+        headingFont: "Lora, sans-serif",
+        bodyFont: "Lora, sans-serif",
+      },
+      fontSize: {
+        huge: "clamp(3.8rem, calc(3.8rem + ((1vw - 0.48rem) * 1.3889)), 4.8rem)",
+        h1: "clamp(2.8rem, calc(2.8rem + ((1vw - 0.48rem) * 1.3889)), 3.8rem)",
+        h2: "clamp(2rem, calc(2rem + ((1vw - 0.48rem) * 1.3889)), 3rem)",
+        h3: "clamp(1.5rem, calc(1.5rem + ((1vw - 0.48rem) * 0.5208)), 1.875rem)",
+        h4: "clamp(1.125rem, calc(1.125rem + ((1vw - 0.48rem) * 0.3472)), 1.375rem)",
+        body: "clamp(1rem, calc(1rem + ((1vw - 0.48rem) * 0.1736)), 1.125rem)",
+        small:
+          "clamp(0.875rem, calc(0.875rem + ((1vw - 0.48rem) * 0.1736)), 1rem)",
+      },
+      lineHeight: {
+        lh1: "1.3",
+        lh2: "1.35",
+        lh3: "1.4",
+        lh4: "1.5",
+        lhbody: "1.6",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addBase, theme }) => {
+      addBase({
+        h1: {
+          fontSize: theme("fontSize.h1"),
+          lineHeight: theme("lineHeight.lh1"),
+        },
+        h2: {
+          fontSize: theme("fontSize.h2"),
+          lineHeight: theme("lineHeight.lh2"),
+        },
+        h3: {
+          fontSize: theme("fontSize.h3"),
+          lineHeight: theme("lineHeight.lh3"),
+        },
+        h4: {
+          fontSize: theme("fontSize.h4"),
+          lineHeight: theme("lineHeight.lh4"),
+        },
+        "label, code, span, li, p, i": {
+          fontFamily: theme("fontFamily.bodyFont"),
+          fontSize: theme("fontSize.body"),
+          fontWeight: "400",
+          lineHeight: theme("lineHeight.lhbody"),
+        },
+        "h1,h2,h3,h4": {
+          fontFamily: theme("fontFamily.headingFont"),
+          fontWeight: "700",
+        },
+      });
+    }),
+  ],
 };
 export default config;
